e0ebf3ce1f76d79b3ed69f8c11bad20161af7e43
Grig <36704039+ferocious-gambit@users.noreply.github.com>
2026-01-27 16:26:39 +0400

    Update 841 Keys and Rooms.py

--- LeetCode 75/Graphs_DFS/841 Keys and Rooms.py
+++ LeetCode 75/Graphs_DFS/841 Keys and Rooms.py
@@ -9,24 +9,15 @@ class Solution:
                 visited_nodes.append(node)
             
                 for key in rooms[node]:
-                    # print(rooms[node])
-                    # print(key)
-                    if key not in visited_nodes:
-                        stack.append(key)
-                # print(stack)
-                # stack = list(set(stack))
+                    stack.append(key)
 
             if stack:
                 dfsRec(stack[-1], visited_nodes, stack)
 
-            return visited_nodes
-            # return set(visited_nodes)
+        visited_nodes = []
+        dfsRec(0, visited_nodes, [0])
 
-        # print(dfsRec(0, [], [0]))
-
-        # print(len(rooms))
-
-        if len(dfsRec(0, [], [0])) == len(rooms):
+        if len(visited_nodes) == len(rooms):
             return True
         
         return False
