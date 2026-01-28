f95e7bedc67c2f4a4396a84edabba877ea956be5
Grig <36704039+ferocious-gambit@users.noreply.github.com>
2026-01-28 22:37:53 +0400

    Update 1926 Nearest Exit from Entrance in Maze.py

---
+++ LeetCode 75/Graphs_BFS/1926 Nearest Exit from Entrance in Maze.py
@@ -0,0 +1,68 @@
+class Solution:
+
+    def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
+        
+        def check_exit(loc):
+            row = loc[0]
+            col = loc[1]
+            if loc != entrance:
+                if row == 0 or row == len(maze)-1 or \
+                    col == 0 or col == len(maze[0])-1:
+                    return True
+
+            return False
+        
+        def check_cell(loc):
+
+            if check_exit(loc):
+                return True
+
+            res = []
+
+            row = loc[0]
+            col = loc[1]
+
+            if row-1 >= 0 and maze[row-1][col] != "+":
+                res.append([row-1, col])
+            if row+1 < len(maze) and maze[row+1][col] != "+":
+                res.append([row+1, col])
+            if col-1 >= 0 and maze[row][col-1] != "+":
+                res.append([row, col-1])
+            if col+1 < len(maze[0]) and maze[row][col+1] != "+":
+                res.append([row, col+1])
+
+            return res
+
+        depth = -1
+        loc = (entrance, 0)
+        que = [loc]
+        visited = []
+        ans = []
+
+        def bfsRec(loc, depth):
+            # print(loc)
+            visited.append(loc[0])
+            que.pop(0)
+            depth+=1
+            res = check_cell(loc)
+            # print(res, depth)
+            if res == True:
+                # print("HHAHAHSHAHA")
+                ans.append(depth)
+            else:
+
+                for loc in res:
+                    if loc not in visited:
+                        que.append(loc)
+
+                if que:
+                    bfsRec(que[0], depth)
+        
+        bfsRec(loc, depth)
+
+        if ans:
+            return ans[0]
+
+        return -1
+
+
