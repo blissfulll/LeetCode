c17451d5dbe2210ba09e1afe94c9d566671427d4
Grig <36704039+ferocious-gambit@users.noreply.github.com>
2026-01-30 15:58:23 +0400

    Update 1926 Nearest Exit from Entrance in Maze.py

--- LeetCode 75/Graphs_BFS/1926 Nearest Exit from Entrance in Maze.py
+++ LeetCode 75/Graphs_BFS/1926 Nearest Exit from Entrance in Maze.py
@@ -1,74 +1,57 @@
 class Solution:
 
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
-        
+    
         def check_exit(loc):
             row = loc[0]
             col = loc[1]
             if loc != entrance:
-                if row == 0 or row == len(maze)-1 or \
-                    col == 0 or col == len(maze[0])-1:
+                if row == 0 or row == len(maze)-1 or col == 0 or col == len(maze[0])-1:
                     return True
             return False
         
-        def check_cell(loc):
+        def add_cells(loc):
 
             depth = loc[1]
             loc = loc[0]
 
-            if check_exit(loc):
-                return True
-
-            res = [()]
+            res = []
 
             row = loc[0]
             col = loc[1]
 
-            if row-1 >= 0 and maze[row-1][col] != "+":
-                res.append(([row-1, col], depth))
-            if row+1 < len(maze) and maze[row+1][col] != "+":
-                res.append(([row+1, col], depth))
-            if col-1 >= 0 and maze[row][col-1] != "+":
-                res.append(([row, col-1], depth))
-            if col+1 < len(maze[0]) and maze[row][col+1] != "+":
-                res.append(([row, col+1], depth))
-
-            return res
+            if row-1 >= 0 and maze[row-1][col] == ".":
+                que.append(([row-1, col], depth+1))
+                maze[row-1][col] = "+"
+            if row+1 < len(maze) and maze[row+1][col] == ".":
+                que.append(([row+1, col], depth+1))
+                maze[row+1][col] = "+"
+            if col-1 >= 0 and maze[row][col-1] == ".":
+                que.append(([row, col-1], depth+1))
+                maze[row][col-1] = "+"
+            if col+1 < len(maze[0]) and maze[row][col+1] == ".":
+                que.append(([row, col+1], depth+1))
+                maze[row][col+1] = "+"
 
         depth = 0
         loc = (entrance, 0)
         que = [loc]
-        visited = []
         ans = []
 
         def bfsRec(loc):
-            # print(loc)
-            visited.append(loc[0])
-            que.pop(0)
-            # depth+=1
-            res = check_cell(loc)
-            # print(res, depth)
-            if res == True:
-                # print("HHAHAHSHAHA")
-                ans.append(loc[1])
-            else:
-                if res == [()]:
-                    pass
-                else:
-                    for loc in res:
-                        print(loc)
-                        if loc[0] not in visited:
-                            que.append((loc[0], loc[1]+1))
+            
+            if check_exit(loc[0]):
+                return loc[1]
 
-                if que:
-                    bfsRec(que[0])
+            add_cells(loc)
+            que.pop(0)                    
+            
+            if que:
+                return bfsRec(que[0])
+                
+            return -1
         
-        bfsRec(loc)
-
-        if ans:
-            return ans[0]
-
-        return -1
+        return bfsRec(loc)
 
 
 
