38aed7f2ab6682c5bfd79932d30c55b514095f02
Grig <36704039+blissfulll@users.noreply.github.com>
2026-02-26 18:24:47 +0400

    Update 875 Koko Eating Bananas.py

--- LeetCode 75/Binary Search/875 Koko Eating Bananas.py
+++ LeetCode 75/Binary Search/875 Koko Eating Bananas.py
@@ -3,21 +3,38 @@ class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         
         def check(min_speed, max_speed):
-            k = min_speed + max_speed // 2
-            count = 0
-            for pile in piles:
-                if pile % k != 0:
-                    count += pile // k + 1
-                else:
-                    count += pile / k
+            k = 0
+            while min_speed < max_speed: 
 
-            if count <= h:
-                return check()
+                k = (min_speed + max_speed) // 2
+                # print(k)
 
-            return k
+                if k == 0:
+                    return k+1
+
+                count = 0
+                for pile in piles:
+                    if pile % k != 0:
+                        count += pile // k + 1
+                    else:
+                        count += pile / k
+
+                # print(count)
+                if count > h:
+                    if min_speed == k:
+                        break
+                    min_speed = k
+                elif count <= h:
+                    if max_speed == k:
+                        break
+                    max_speed = k
+
+            return k+1
 
         min_speed = 0
         max_speed = max(piles)
 
-        check(min_speed, max_speed)
-            
+        if len(piles) == h:
+            return max_speed
+        
+        return check(min_speed, max_speed)
